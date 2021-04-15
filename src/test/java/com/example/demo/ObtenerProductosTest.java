3eebe4a977dc807a64867e558637dfb201a64e6f
ArturoAguilarL <arturo.aguilar@mercadolibre.com>
2021-04-15 16:26:23 -0300

    limpio el proyecto

--- src/test/java/com/example/demo/ObtenerProductosTest.java
+++ src/test/java/com/example/demo/ObtenerProductosTest.java
@@ -1,15 +1,10 @@
 package com.example.demo;
 
 
-import com.example.demo.repositories.MeliRepository;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ObtenerProductosTest {
-
-    @Mock
-    private MeliRepository meliRepository;
-
 
 }
